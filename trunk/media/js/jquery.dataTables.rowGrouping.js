719fc472449690868f8ebf9ea8bd29caa1e80e4d
jocapc@gmail.com <jocapc@gmail.com@b13adf85-0c9f-597a-68eb-54bd78b49e31>
2011-06-04 19:49:29 +0000

    Added expandable collapsible grouping

--- trunk/media/js/jquery.dataTables.rowGrouping.js
+++ trunk/media/js/jquery.dataTables.rowGrouping.js
@@ -1,6 +1,6 @@
 /*
 * File:        jquery.dataTables.grouping.js
-* Version:     0.1.
+* Version:     0.2.
 * Author:      Jovan Popovic 
 * 
 * Copyright 2011 Jovan Popovic, all rights reserved.
@@ -11,6 +11,32 @@
 * This source file is distributed in the hope that it will be useful, but 
 * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 * or FITNESS FOR A PARTICULAR PURPOSE. 
+* Parameters:
+* @iGroupingColumnIndex                                 Integer             Index of the column that will be used for grouping - default 0
+* @sGroupingColumnSortDirection                         Enumeration         Sort direction of the group
+* @iGroupingOrderByColumnIndex                          Integer             Index of the column that will be used for ordering groups
+* @sGrupingClass                                        String              Class that will be associated to the group row. Default - "group"
+* @bSetGroupingClassOnTR                                Boolean             If set class will be set to the TR instead of the TD withing the grouping TR
+* @bHideGroupingColumn                                  Boolean             Hide column used for grouping once results are grouped. Default - true
+* @bHideGroupingOrderByColumn                           Boolean             Hide column used for ordering groups once results are grouped. Default - true
+* @sGroupBy                                             Enumeration         Type of grouping that should be applied. Values "name"(default), "letter", "year"
+* @sGroupLabelPrefix                                    String              Prefix that will be added to each group cell
+* @bExpandableGrouping                                  Boolean             Attach expand/collapse handlers to the grouping rows
+* @bExpandSingleGroup                                   Boolean             Use accordon grouping
+* @iExpandGroupOffset                                   Integer             Number of pixels to set scroll position above the currently selected group. If -1 scroll will be alligned to the table
+* General settings
+* @sDateFormat: "dd/MM/yyyy"                            String              Date format used for grouping
+* @sEmptyGroupLabel                                     String              Lable that will be placed as group if grouping cells are empty. Default "-"
+
+* Parameters used in the second level grouping
+* @iGroupingColumnIndex2                                Integer             Index of the secondary column that will be used for grouping - default 0
+* @sGroupingColumnSortDirection2                        Enumeration         Sort direction of the secondary group
+* @iGroupingOrderByColumnIndex2                         Integer             Index of the column that will be used for ordering secondary groups
+* @sGrupingClass2                                       String              Class that will be associated to the secondary group row. Default "subgroup"
+* @bHideGroupingColumn2                                 Boolean             Hide column used for secondary grouping once results are grouped. Default - true,
+* @bHideGroupingOrderByColumn2                          Boolean             Hide column used for ordering secondary groups once results are grouped. Default - true,
+* @sGroupBy2                                            Enumeration         Type of grouping that should be applied to secondary column. Values "name"(default), "letter", "year",
+* @sGroupLabelPrefix2                                   String              Prefix that will be added to each secondary group cell
 * 
 */
 (function ($) {
@@ -50,18 +76,24 @@
             bHideGroupingColumn: true,
             bHideGroupingOrderByColumn: true,
             sGroupBy: "name",
+            sGroupLabelPrefix: "",
+            bExpandableGrouping: false,
+            bExpandSingleGroup: false,
+            iExpandGroupOffset: 100,
 
             sDateFormat: "dd/MM/yyyy",
             sEmptyGroupLabel: "-",
             bSetGroupingClassOnTR: false,
 
             iGroupingColumnIndex2: -1,
-	    sGroupingColumnSortDirection2: "",
-	    iGroupingOrderByColumnIndex2: -1,
+            sGroupingColumnSortDirection2: "",
+            iGroupingOrderByColumnIndex2: -1,
             sGrupingClass2: "subgroup",
             bHideGroupingColumn2: true,
             bHideGroupingOrderByColumn2: true,
             sGroupBy2: "name",
+            sGroupLabelPrefix2: "",
+            bExpandableGrouping2: false
 
 
         };
@@ -150,8 +182,14 @@
                     }
 
                     if (sLastGroup == null || sGroup != sLastGroup) {
+                        if (properties.bExpandableGrouping && sLastGroup == null)
+                            properties.sCurrentGroup = sGroup;
+
+
                         var nGroup = document.createElement('tr');
                         var nCell = document.createElement('td');
+
+                        nGroup.id = "group-id-" + sGroup.toLowerCase().replace(" ", "-");
 
                         if (properties.bSetGroupingClassOnTR) {
                             nGroup.className = properties.sGrupingClass + " " + sGroup.toLowerCase().replace(" ", "-");
@@ -160,16 +198,95 @@
                         }
 
                         nCell.colSpan = iColspan;
-                        nCell.innerHTML = sGroup == "" ? properties.sEmptyGroupLabel : sGroup;
+                        nCell.innerHTML = properties.sGroupLabelPrefix + (sGroup == "" ? properties.sEmptyGroupLabel : sGroup);
+                        if (properties.bExpandableGrouping) {
+                            if (properties.bExpandSingleGroup) {
+                                if (properties.sCurrentGroup != sGroup) {
+                                    nCell.className += " group-item-expander collapsed-group";
+                                } else {
+                                    nCell.className += " group-item-expander expanded-group";
+                                }
+                            } else {
+                                nCell.className += " group-item-expander expanded-group";
+                            }
+                            nCell.rel = sGroup.toLowerCase().replace(" ", "-");
+                            //nCell.innerHTML += "<a href='#' class='group-item-expander expanded-group' rel='" + sGroup + "' >TEST</a>";
 
+                            ///*************
+
+
+                            $(nCell).click(function (e) {
+
+
+                                var sGroup = $(this).attr("rel");
+                                var sCurrentGroup = properties.sCurrentGroup.toLowerCase().replace(" ", "-");
+                                var bIsExpanded = $(this).hasClass("expanded-group");
+                                if (properties.bExpandSingleGroup) {
+                                    if (!bIsExpanded) {
+
+                                        $("tr.group-item-" + sCurrentGroup).hide();
+                                        $("tr.group-item-" + sGroup).show();
+
+                                        var oTrExpandedGroup = $(".expanded-group");
+                                        oTrExpandedGroup.removeClass("expanded-group");
+                                        oTrExpandedGroup.addClass("collapsed-group");
+                                        $(this).addClass("expanded-group");
+                                        $(this).removeClass("collapsed-group");
+                                        properties.sCurrentGroup = sGroup;
+                                        if (properties.iExpandGroupOffset != -1) {
+                                            var position = $("#group-id-" + sGroup).offset().top - properties.iExpandGroupOffset;
+                                            window.scroll(0, position);
+                                        } else {
+                                            var position = oTable.offset().top;
+                                            window.scroll(0, position);
+                                        }
+                                    }
+                                } else {
+                                    if (bIsExpanded) {
+                                        $(this).removeClass("expanded-group");
+                                        $(this).addClass("collapsed-group");
+                                        $("tr.group-item-" + sGroup).hide();
+                                    } else {
+                                        $(this).addClass("expanded-group");
+                                        $(this).removeClass("collapsed-group");
+                                        $("tr.group-item-" + sGroup).show();
+                                    }
+                                }
+                                e.preventDefault();
+
+                            }
+                    );
+
+
+
+
+
+
+                            ///***********
+
+
+
+
+
+
+                        }
                         nGroup.appendChild(nCell);
                         nTrs[i].parentNode.insertBefore(nGroup, nTrs[i]);
                         sLastGroup = sGroup;
-                        sLastGroup2 = null;//to reset second level grouping
+                        sLastGroup2 = null; //to reset second level grouping
+
+
 
 
 
                     } // end if (sLastGroup == null || sGroup != sLastGroup)
+
+
+                    if (properties.bExpandableGrouping && properties.bExpandSingleGroup) {
+                        if (sGroup != properties.sCurrentGroup) {
+                            $(nTrs[i]).hide();
+                        }
+                    }
 
                     if (bUseSecondaryGrouping) {
 
@@ -184,7 +301,7 @@
                             }
 
                             nCell2.colSpan = iColspan;
-                            nCell2.innerHTML = sGroup2 == "" ? properties.sEmptyGroupLabel : sGroup2;
+                            nCell2.innerHTML = properties.sGroupLabelPrefix2 + (sGroup2 == "" ? properties.sEmptyGroupLabel : sGroup2);
 
                             nGroup2.appendChild(nCell2);
                             nTrs[i].parentNode.insertBefore(nGroup2, nTrs[i]);
@@ -194,14 +311,26 @@
 
                     } //end if (bUseSecondaryGrouping)
 
+                    if (properties.bExpandableGrouping) {
+
+                        $(nTrs[i]).addClass("group-item-" + sGroup.toLowerCase().replace(" ", "-"));
+                    }
 
                 } // end for (var i = 0; i < nTrs.length; i++)
 
 
                 //-----End grouping
 
+                //Add click handlers on the groups
+                if (properties.bExpandableGrouping) {
+
+
+                }
 
             };
+
+
+
 
             oTable.fnSetColumnVis(properties.iGroupingColumnIndex, !properties.bHideGroupingColumn);
             if (properties.bCustomColumnOrdering) {
@@ -218,15 +347,15 @@
                 "sName": "fnRowGroupung"
             });
 
-	    var aaSortingFixed = new Array();
-	    aaSortingFixed.push( [properties.iGroupingOrderByColumnIndex, properties.sGroupingColumnSortDirection] );
+            var aaSortingFixed = new Array();
+            aaSortingFixed.push([properties.iGroupingOrderByColumnIndex, properties.sGroupingColumnSortDirection]);
             if (properties.iGroupingColumnIndex2 != -1) {
-                aaSortingFixed.push( [properties.iGroupingOrderByColumnIndex2, properties.sGroupingColumnSortDirection2] );
+                aaSortingFixed.push([properties.iGroupingOrderByColumnIndex2, properties.sGroupingColumnSortDirection2]);
             }
- 
-	    oTable.fnSettings().aaSortingFixed = aaSortingFixed;          
-	    //Old way
-	    //oTable.fnSettings().aaSortingFixed = [[properties.iGroupingOrderByColumnIndex, properties.sGroupingColumnSortDirection]];
+
+            oTable.fnSettings().aaSortingFixed = aaSortingFixed;
+            //Old way
+            //oTable.fnSettings().aaSortingFixed = [[properties.iGroupingOrderByColumnIndex, properties.sGroupingColumnSortDirection]];
 
             switch (properties.sGroupBy) {
                 case "name":
@@ -278,6 +407,9 @@
             }
 
             oTable.fnDraw();
+
+
+
 
 
         });
